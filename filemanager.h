61336f588fcd55c0fd1250a548ad422975ffe714
AABCEH112358 <aneskhacha99@gmail.com>
2026-06-10 13:17:45 -0600

    Final implementation before the beta

---
+++ filemanager.h
@@ -0,0 +1,22 @@
+ifndef FILEMANAGER_H
+#define FILEMANAGER_H
+
+#include <string>
+#include <vector>
+#include "reservation.h"
+
+class TheatreShow;
+
+class FileManager{
+    public:
+
+        FileManager(); 
+
+        int loadShows(const std::string& filename, std::vector<TheatreShow>& shows);
+        
+        int loadReservations(const std::string& filename, std::vector<TheatreShow>& shows);
+
+        int saveReservations(const std::string& filename, const std::vector<TheatreShow>& shows) const;
+};
+
+#endif
